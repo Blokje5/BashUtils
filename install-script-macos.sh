d852fdde398a032dfe603a1e6de7a62812f5da8b
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2018-04-15 18:38:54 +0200

    Basic installation script finished. Need some more time to figure out which programs I want to install. Next up config + dotfiles

--- install-script-macos.sh
+++ install-script-macos.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-#Source utilities
+# Setup environment variables
+VERBOSITY=3 #INFO
+LOGFILE="~/.installation_logfile.txt"
+FORCED=1
+
+# Source utilities
 for f in $(pwd)/lib/*.sh;
 do
     source $f;
@@ -8,5 +13,82 @@ done;
 
 info "This script will setup a new OSX environment"
 
+# installing ruby before brew
+install_ruby
+
 # installing brew
 install_brew
+
+# installing brews
+function installHomebrewPackages() {
+    unset LISTINSTALLED INSTALLCOMMAND RECIPES
+
+    notice "Checking for Homebrew packages to install..."
+
+    LISTINSTALLED="brew list"
+    INSTALLCOMMAND="brew install"
+
+    RECIPES=(
+        awscli
+        git
+        jq
+        node
+        sbt@1
+        wget
+    )
+    doInstall
+
+    info "Done installing Homebrew packages"
+}
+
+installHomebrewPackages
+
+# installing casks
+function installCaskApps() {
+    unset LISTINSTALLED INSTALLCOMMAND RECIPES
+
+    info "Checking for casks to install..."
+
+    LISTINSTALLED="brew cask list"
+    INSTALLCOMMAND="brew cask install --appdir=/Applications"
+    RECIPES=(
+        docker
+        firefox
+        flux
+        google-chrome
+        intellij-idea
+        iterm2
+        java
+        postman
+        python
+        visual-studio-code
+        visualvm
+    )
+
+    # for item in "${RECIPES[@]}"; do
+    #   info "$item"
+    # done
+    doInstall
+
+    info "Done installing cask apps"
+}
+
+installCaskApps
+
+# installing npm libraries
+function installNPMLibraries() {
+    unset LISTINSTALLED INSTALLCOMMAND RECIPES
+
+    info "installing global npm packages"
+
+    LISTINSTALLED="npm list -g --depth 0"
+    INSTALLCOMMAND="npm install -g"
+    RECIPES=(
+        typescript
+        @angular/cli
+    )
+
+    info "Done installing npm libraries"
+}
+
+installNPMLibraries
---
+++ lib/install-utils.sh
@@ -0,0 +1,235 @@
+#!/usr/bin/env bash
+
+# ##################################################
+# Install utilities for Bash
+#
+# VERSION 1.0.0
+#
+# HISTORY
+#
+# * 2018-04-03 - v1.0.0  - Initial version
+#
+# CONTRIBUTORS
+#
+# * Lennard Eijsackers
+#
+# DEPENDENCIES
+#
+# * command-util.sh
+# * logger.sh
+# * cli-utils.sh
+# ##################################################
+
+# Usage: Check if brew exists
+# Returns: Boolean
+brew_installed() {
+    if $(command_exists 'brew');
+        then echo true
+    else
+        echo false    
+    fi    
+}
+
+# Usage: maintain brew
+brewMaintenance () {
+  forceable_confirmation "Run Homebrew maintenance?"
+  if is_confirmed; then
+    brew doctor
+    brew update
+    brew upgrade --all
+  fi
+}
+
+# Usage: Installs homebrew and its needed dependecies
+install_brew() {
+    info "Starting brew installation process"
+    if $(brew_installed);
+    then
+        info "Brew is already installed"
+    else
+        debug "seeking confirmation for brew installation"
+        forceable_confirmation "Install Homebrew?"
+        if $(is_confirmed); 
+        then
+            installXCodeCLI
+            trace "Homebrew install"
+            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            trace "Homebrew install ended"
+            trace "tapping brews"
+            tapBrews
+            trace "brews tapped"
+        else
+            # TODO neat exit
+            debug "user declined installation"
+            exit 1
+        fi 
+    fi
+}
+
+# Usage checks for the xcode cli tools
+installXCodeCLI() {
+    if [[ ! $(command_exists 'gcc') ]];
+    then
+        debug "seeking confirmation for Xcode installation"
+        info "XCode is needed before Homebrew can be installed"
+        forceable_confirmation "Install XCode?"
+        if $(is_confirmed); 
+        then
+            trace "Xcode install"
+            xcode-select --install
+            trace "Xcode install ended"
+        else
+            # TODO neat exit
+            debug "user declined installation"
+            exit 1
+        fi
+    fi    
+}
+
+# Usage: Tap into more brews
+tapBrews() {
+    brew tap caskroom/cask
+}
+
+# Usage: install ruby
+install_ruby() {
+    info "Checking for RVM (Ruby Version Manager)..."
+
+    local RUBYVERSION="2.1.2" # Version of Ruby to install via RVM
+
+    # Check for RVM
+    if [ ! "$(command_exists rvm)" ]; then
+        forceable_confirmation "Couldn't find RVM. Install it?"
+        if is_confirmed; then
+        curl -L https://get.rvm.io | bash -s stable
+        source "${HOME}/.rvm/scripts/rvm"
+        source "${HOME}/.bash_profile"
+        #rvm get stable --autolibs=enable
+        rvm install ${RUBYVERSION}
+        rvm use ${RUBYVERSION} --default
+        fi
+    fi
+
+    success "RVM and Ruby are installed"
+}
+
+  function doInstall () {
+    # Reads a list of items, checks if they are installed, installs
+    # those which are needed.
+    #
+    # Variables needed are:
+    # LISTINSTALLED:  The command to list all previously installed items
+    #                 Ex: "brew list" or "gem list | awk '{print $1}'"
+    #
+    # INSTALLCOMMAND: The Install command for the desired items.
+    #                 Ex:  "brew install" or "gem install"
+    #
+    # RECIPES:      The list of packages to install.
+    #               Ex: RECIPES=(
+    #                     package1
+    #                     package2
+    #                   )
+    #
+    # Credit: https://github.com/cowboy/dotfiles
+    # Credit: https://github.com/natelandau/shell-scripts
+    function isAppInstalled() {
+    # Feed this function either the bundleID (com.apple.finder) or a name (finder) for a native
+    # mac app and it will determine whether it is installed or not
+    #
+    # usage: if isAppInstalled 'finder' &>/dev/null; then ...
+    #
+    # http://stackoverflow.com/questions/6682335/how-can-check-if-particular-application-software-is-installed-in-mac-os
+
+  local appNameOrBundleId="$1" isAppName=0 bundleId
+    # Determine whether an app *name* or *bundle ID* was specified.
+    [[ $appNameOrBundleId =~ \.[aA][pP][pP]$ || $appNameOrBundleId =~ ^[^.]+$ ]] && isAppName=1
+    if (( isAppName )); then # an application NAME was specified
+      # Translate to a bundle ID first.
+      bundleId=$(osascript -e "id of application \"$appNameOrBundleId\"" 2>/dev/null) ||
+        { echo "$FUNCNAME: ERROR: Application with specified name not found: $appNameOrBundleId" 1>&2; return 1; }
+    else # a BUNDLE ID was specified
+      bundleId=$appNameOrBundleId
+    fi
+      # Let AppleScript determine the full bundle path.
+    osascript -e "tell application \"Finder\" to POSIX path of (get application file id \"$bundleId\" as alias)" 2>/dev/null ||
+      { echo "$FUNCNAME: ERROR: Application with specified bundle ID not found: $bundleId" 1>&2; return 1; }
+    }
+
+    function to_install() {
+      local desired installed i desired_s installed_s remain
+      # Convert args to arrays, handling both space- and newline-separated lists.
+      read -ra desired < <(echo "$1" | tr '\n' ' ')
+      read -ra installed < <(echo "$2" | tr '\n' ' ')
+      # Sort desired and installed arrays.
+      unset i; while read -r; do desired_s[i++]=$REPLY; done < <(
+        printf "%s\n" "${desired[@]}" | sort
+      )
+      unset i; while read -r; do installed_s[i++]=$REPLY; done < <(
+        printf "%s\n" "${installed[@]}" | sort
+      )
+      # Get the difference. comm is awesome.
+      unset i; while read -r; do remain[i++]=$REPLY; done < <(
+        comm -13 <(printf "%s\n" "${installed_s[@]}") <(printf "%s\n" "${desired_s[@]}")
+      )
+      echo "${remain[@]}"
+    }
+
+    function checkInstallItems() {
+      # If we are working with 'cask' we need to dedupe lists
+      # since apps might be installed by hand
+      if [[ $INSTALLCOMMAND =~ cask ]]; then
+        if isAppInstalled "${item}" &>/dev/null; then
+          continue
+        fi
+      fi
+      # If we installing from mas (mac app store), we need to dedupe the list AND
+      # sign in to the app store
+      if [[ $INSTALLCOMMAND =~ mas ]]; then
+        # Lookup the name of the application being installed
+        appName="$(curl -s https://itunes.apple.com/lookup?id=$item | jq .results[].trackName)"
+        if isAppInstalled "${appName}" &> /dev/null; then
+          continue
+        fi
+      # Tell the user the name of the app
+      info "$item --> $appName"
+      fi
+    }
+
+    # Log in to the Mac App Store if using mas
+    if [[ $INSTALLCOMMAND =~ mas ]]; then
+        mas signout
+        input "Please enter your Mac app store username: "
+        read macStoreUsername
+        input "Please enter your Mac app store password: "
+        read -s macStorePass
+        echo ""
+        mas signin $macStoreUsername "$macStorePass"
+    fi
+
+    list=($(to_install "${RECIPES[*]}" "$(${LISTINSTALLED})"))
+
+    if [ ${#list[@]} -gt 0 ]; then
+      forceable_confirmation "Confirm each package before installing?"
+      if is_confirmed; then
+        for item in "${list[@]}"; do
+          checkInstallItems
+          forceable_confirmation "Install ${item}?"
+          if is_confirmed; then
+            info "Installing ${item}"
+            elif [[ "${item}" = "tldr" ]]; then
+              brew tap tldr-pages/tldr
+              brew install tldr
+            else
+              ${INSTALLCOMMAND} "${item}"
+            fi
+          fi
+        done
+      else
+        for item in "${list[@]}"; do
+          checkInstallItems
+          info "Installing ${item}"
+          ${INSTALLCOMMAND} "${item}"
+        done
+      fi
+    fi
+  }
